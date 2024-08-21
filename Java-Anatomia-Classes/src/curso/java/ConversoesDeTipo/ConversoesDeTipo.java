7b44e827caad1bc740056860f4358e1c1de3c48b
Luiz Henrique <luizhenrique204061@gmail.com>
2024-08-21 01:40:59 -0300

    Segundo Commit

---
+++ Java-Anatomia-Classes/src/curso/java/ConversoesDeTipo/ConversoesDeTipo.java
@@ -0,0 +1,49 @@
+package curso.java.ConversoesDeTipo;
+
+public class ConversoesDeTipo {
+    public static void main(String[] args) {
+        
+        //Valor String
+        String valorDoSalarioTexto = "2250.45";
+
+        System.out.println("Texto " + valorDoSalarioTexto);
+
+        //Valor Double
+        double valorDoSalarioDouble = Double.parseDouble(valorDoSalarioTexto);
+
+        System.out.println("Double " + valorDoSalarioDouble);
+
+        //Valor Int
+        int valorDoSalarioInteiro = (int) valorDoSalarioDouble;
+
+        System.out.println("Inteiro " + valorDoSalarioInteiro);
+        
+        //Voltando a String
+        
+        String voltandoStringInt = String.valueOf(valorDoSalarioInteiro);
+
+        System.out.println("Inteiro para String " + voltandoStringInt);
+
+        String voltandoStringDouble = Double.toString(valorDoSalarioDouble);
+
+        System.out.println("Double para String " + voltandoStringDouble);
+
+        //Conversão de Double para Int e vice versa
+
+        double tipoDouble = 50.42;
+
+        System.out.println("Tipo Double " + tipoDouble);
+
+        int tipoInt = (int) tipoDouble;
+
+        System.out.println(tipoInt);
+
+        int segundoTipoInt = 100;
+
+        System.out.println("Tipo Int " + segundoTipoInt);
+
+        double segundoTipoDouble = (double) segundoTipoInt;
+
+        System.out.println(segundoTipoDouble);
+    }
+}
---
+++ Java-Anatomia-Classes/src/curso/java/ConversoesDeTipo/tiposdevariaveis/TiposDeVariaveis.java
@@ -0,0 +1,59 @@
+package curso.java.ConversoesDeTipo.tiposdevariaveis;
+
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2.500;
+
+        // int salarioInteiro = (int) salarioMinimo;
+
+        int salarioInteiro = (int) salarioMinimo;
+
+        System.out.println("Salário Inteiro " + salarioInteiro);
+
+        String salarioString = String.valueOf(salarioMinimo);
+
+
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        System.out.println("Salário String " + salarioString);
+
+        System.out.println(salarioMinimo);
+
+
+        int numero = 1;
+
+        numero = 2;
+
+        String valorString = "30.40";
+
+        double stringConvertida = Double.parseDouble(valorString);
+
+        System.out.println(stringConvertida);
+
+
+        //Convertendo de Int para Double
+
+        int dez = 10;
+
+
+        System.out.println(dez);
+
+        double dezDecimal = (int) dez;
+
+        System.out.println(dezDecimal);
+
+        //Valor Nunca Poderá Mudar
+
+        final double VALOR_DE_PI = 3.14;
+
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
