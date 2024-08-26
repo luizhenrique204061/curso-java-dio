d46def899501bae5ea13f849522e4ca5ba32b625
Luiz Henrique <luizhenrique204061@gmail.com>
2024-08-26 00:58:16 -0300

    Decimo Segundo Commit

--- Scanner-Entrada-Java-Exception/src/ScannerEntradaException.java
+++ Scanner-Entrada-Java-Exception/src/ScannerEntradaException.java
@@ -5,6 +5,8 @@ public class ScannerEntradaException {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        String rodera = "Rodera";
+
         double altura = 0.0;
         String nome = "";
         String sobrenome = "";
