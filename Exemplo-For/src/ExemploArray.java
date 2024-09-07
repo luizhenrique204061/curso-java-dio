ae09f8e1372e6f373552e1a647c36d7e53cf19e7
Luiz Henrique <luizhenrique204061@gmail.com>
2024-09-07 01:41:34 -0300

    Decimo Quinto Commit

--- Exemplo-For/src/ExemploArray.java
+++ Exemplo-For/src/ExemploArray.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class ExemploArray {
     public static void main(String[] args) {
         String alunos [] = {"FELIPE", "JONAS", "JÚLIA", "MARCOS"};
 
+        System.out.println(Arrays.toString(alunos));
+
         //For
         for (int x = 0; x < alunos.length; x++){
-            System.out.println("O aluno no índicie x=" + x + " é " + alunos[x]);
+            System.out.println("O aluno no índice x=" + x + " é " + alunos[x]);
         }
 
         System.out.println();
